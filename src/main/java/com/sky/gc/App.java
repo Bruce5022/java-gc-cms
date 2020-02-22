7bdb6eccdf980c5f84c7dd03b63ebadba262bd47
szw <shizhanwei123@aliyun.com>
2020-02-23 01:46:50 +0800

    cms垃圾回收器学习

--- src/main/java/com/sky/gc/App.java
+++ src/main/java/com/sky/gc/App.java
@@ -8,10 +8,10 @@ public class App {
         int size = 1024*1024;
 
         byte[] myAlloc1 = new byte[4*size];
-        // 第一次在新生代中申请了4M的内存，并没有执行GC操作，因为我们新生代Eden空间的大小是8M，完全能容纳下
+
         System.out.println("111111");
 
-        // 接着再申请4M的大小，此时就发生了GC，很明显新生代已经容纳不下了
+
         byte[] myAlloc2 = new byte[4*size];
         System.out.println("222222");
 
